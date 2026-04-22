ce0090b380eae17bf273a3904df789f9717ab6a5
raulgonzalez01 <raul.01gonzalez@hotmail.com>
2026-04-22 14:56:48 -0600

    Proyecto creado

---
+++ src/main/java/com/ApiPokemonService/ApiPokemonService/ApiPokemonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ApiPokemonService.ApiPokemonService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPokemonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPokemonServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ApiPokemonService/ApiPokemonService/ApiPokemonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiPokemonService.ApiPokemonService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPokemonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
